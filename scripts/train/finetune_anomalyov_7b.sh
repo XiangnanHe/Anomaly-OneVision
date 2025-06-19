1aa7becef8e88f0cf37dbce4da586e55dbe4de36
shawn_he <xiangnan_he@apple.com>
2025-06-19 11:18:02 -0700

    update 7b train

--- scripts/train/finetune_anomalyov_7b.sh
+++ scripts/train/finetune_anomalyov_7b.sh
@@ -29,13 +29,13 @@ PREV_STAGE_CHECKPOINT="lmms-lab/llava-onevision-qwen2-7b-ov"
 echo "PREV_STAGE_CHECKPOINT: ${PREV_STAGE_CHECKPOINT}"
 echo "MID_RUN_NAME: ${RUN_NAME}"
 
-NUM_GPUS=8
+NUM_GPUS=4
 NNODES=1
 RANK=0
 ADDR=127.0.0.1
 PORT=29505
 
-ACCELERATE_CPU_AFFINITY=1 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NNODES}" --node_rank="${RANK}" --master_addr="${ADDR}" --master_port="${PORT}" \
+ACCELERATE_CPU_AFFINITY=1 CUDA_VISIBLE_DEVICES=4,5,6,7 torchrun --nproc_per_node="${NUM_GPUS}" --nnodes="${NNODES}" --node_rank="${RANK}" --master_addr="${ADDR}" --master_port="${PORT}" \
     llava/train/train_mem.py \
     --deepspeed scripts/zero3.json \
     --model_name_or_path $PREV_STAGE_CHECKPOINT \
